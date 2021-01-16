ebd195d01d9de0366b717a5d212aacb70d8b3549
pongvarid <pongvarid@gmail.com>
2021-01-17 01:40:21 +0700

    add release

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   "publicPath": "",
+
   "pluginOptions": {
     "cordovaPath": "src-cordova"
   },
+
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ?
-      '/buffalo_market_web/' :
-      '/'
 
+   
 }
